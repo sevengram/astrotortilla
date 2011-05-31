2f3438f4bd6f4a58619773beecd1a0867cd376b2
naavis <naavis@2feaa585-53de-4bc4-adf1-b5f9a0ce27d5>
2011-05-31 21:50:48 +0000

    Fixed a bug with the downscale parameter.

--- astromate/solver/AstrometryNetSolver.py
+++ astromate/solver/AstrometryNetSolver.py
@@ -141,8 +141,8 @@ class AstrometryNetSolver(IPlateSolver):
         options.append("-L %s"%(minFov or self.getProperty("scale_low")))
         options.append("-H %s"%(maxFov or self.getProperty("scale_max")))
         options.append("-u %s"%(self.getProperty("scale_units")))
-	if self.getProperty("downscale") > 1:
-		options.append("-z %d"%self.getProperty("downscale"))
+        if self.getProperty("downscale") > 1:
+            options.append("-z %s"%self.getProperty("downscale"))
 
 
         r=self.__execute('solve-field %s --no-plot -D `cygpath %s` `cygpath %s`'%(" ".join(options), workDir, imagePath))
